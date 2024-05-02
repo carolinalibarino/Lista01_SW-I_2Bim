668b6583943faf474d49c4b9e1588a866c1549bb
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-02 11:16:51 -0300

    Add files via upload

---
+++ exe01/exe01/exe01/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe01
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, decimal preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public void AdicionarItem(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverItem(int quantidade)
+        {
+            if (Quantidade >= quantidade)
+            {
+                Quantidade -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade insuficiente em estoque.");
+            }
+        }
+    }
+}
---
+++ exe01/exe01/exe01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exe01
+{
+    internal class Program
+    {
+        class Programa
+        {
+            static void Main(string[] args)
+            {
+                Produto produto1 = new Produto("Camiseta", 25.00m, 50);
+                Console.WriteLine($"Produto: {produto1.Nome}, Preço: R${produto1.Preco}, Quantidade em estoque: {produto1.Quantidade}");
+
+                produto1.AdicionarItem(10);
+                Console.WriteLine($"Quantidade após adicionar: {produto1.Quantidade}");
+
+                produto1.RemoverItem(5);
+                Console.WriteLine($"Quantidade após remover: {produto1.Quantidade}");
+            }
+        }
+    }
+}
