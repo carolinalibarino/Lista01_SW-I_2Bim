27148c8c1a79b9bf811d40ce75c28d0960d66bc5
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-02 11:21:25 -0300

    Add files via upload

---
+++ exe02/exe02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exe02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistroDeCompras registro = new RegistroDeCompras();
+            Produto produto1 = new Produto("Camiseta", 25.00m, 50);
+            Produto produto2 = new Produto("Calça", 50.00m, 30);
+
+            registro.AdicionarCompra(DateTime.Now, produto1, 500.00m);
+            registro.AdicionarCompra(DateTime.Now.AddDays(-1), produto2, 1000.00m);
+
+            registro.ListarCompras();
+        }
+    }
+}
---
+++ exe02/exe02/RegistoCompras.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe02
+{
+    class RegistroDeCompras
+    {
+        private List<(DateTime data, Produto produto, decimal valor)> compras;
+
+        public RegistroDeCompras()
+        {
+            compras = new List<(DateTime, Produto, decimal)>();
+        }
+
+        public void AdicionarCompra(DateTime data, Produto produto, decimal valor)
+        {
+            compras.Add((data, produto, valor));
+        }
+
+        public void ListarCompras()
+        {
+            foreach (var compra in compras)
+            {
+                Console.WriteLine($"Data: {compra.data}, Produto: {compra.produto.Nome}, Valor: R${compra.valor}");
+            }
+        }
+    }
+
+    class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, decimal preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+    }
+}
