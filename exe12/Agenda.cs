39e20af8e7d7a7905ea145aaba4ed7d1bbe3e3f9
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-16 11:32:12 -0300

    Add files via upload

---
+++ exe12/Agenda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe12
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public float Altura { get; set; }
+
+        public Pessoa(string nome, int idade, float altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+    }
+
+    public class Agenda
+    {
+        private List<Pessoa> pessoas = new List<Pessoa>();
+
+        public void ArmazenaPessoa(string nome, int idade, float altura)
+        {
+            pessoas.Add(new Pessoa(nome, idade, altura));
+        }
+
+        public void RemovePessoa(string nome)
+        {
+            pessoas.RemoveAll(p => p.Nome == nome);
+        }
+
+        public Pessoa BuscaPessoa(string nome)
+        {
+            return pessoas.Find(p => p.Nome == nome);
+        }
+
+        public void ImprimeAgenda()
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, Altura: {pessoa.Altura}");
+            }
+        }
+    }
+}
---
+++ exe12/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace exe12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Armazenar Pessoa");
+                Console.WriteLine("2. Remover Pessoa");
+                Console.WriteLine("3. Buscar Pessoa");
+                Console.WriteLine("4. Imprimir Agenda");
+                Console.WriteLine("5. Sair");
+
+                Console.Write("Escolha uma opção: ");
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Console.Write("Altura: ");
+                        float altura = float.Parse(Console.ReadLine());
+                        agenda.ArmazenaPessoa(nome, idade, altura);
+                        break;
+                    case 2:
+                        Console.Write("Nome da pessoa a ser removida: ");
+                        string nomeRemover = Console.ReadLine();
+                        agenda.RemovePessoa(nomeRemover);
+                        break;
+                    case 3:
+                        Console.Write("Nome da pessoa a ser buscada: ");
+                        string nomeBuscar = Console.ReadLine();
+                        Pessoa pessoaEncontrada = agenda.BuscaPessoa(nomeBuscar);
+                        if (pessoaEncontrada != null)
+                        {
+                            Console.WriteLine($"Pessoa encontrada: Nome: {pessoaEncontrada.Nome}, Idade: {pessoaEncontrada.Idade}, Altura: {pessoaEncontrada.Altura}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pessoa não encontrada na agenda.");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Agenda:");
+                        agenda.ImprimeAgenda();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saindo...");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
