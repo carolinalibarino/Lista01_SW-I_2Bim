b0e281bb7c00c3ee5e000dc78aa658f1b399aa1e
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-16 08:42:35 -0300

    Add files via upload

---
+++ exe04/AgendaTelefonica.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe04
+{
+    class Contato
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+        public Contato(string nome, string telefone, string email)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Email = email;
+        }
+    }
+
+    class AgendaTelefonica
+    {
+        private List<Contato> contatos;
+
+        public AgendaTelefonica()
+        {
+            contatos = new List<Contato>();
+        }
+
+        public void AdicionarContato(string nome, string telefone, string email)
+        {
+            Contato contato = new Contato(nome, telefone, email);
+            contatos.Add(contato);
+            Console.WriteLine("Contato adicionado com sucesso!");
+        }
+
+        public void RemoverContato(string nome)
+        {
+            Contato contato = contatos.Find(c => c.Nome == nome);
+            if (contato != null)
+            {
+                contatos.Remove(contato);
+                Console.WriteLine("Contato removido com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Contato não encontrado.");
+            }
+        }
+
+        public void BuscarContato(string nome)
+        {
+            Contato contato = contatos.Find(c => c.Nome == nome);
+            if (contato != null)
+            {
+                Console.WriteLine($"Nome: {contato.Nome}, Telefone: {contato.Telefone}, Email: {contato.Email}");
+            }
+            else
+            {
+                Console.WriteLine("Contato não encontrado.");
+            }
+        }
+    }
+
+}
---
+++ exe04/Program.cs
@@ -0,0 +1,49 @@
+﻿using exe04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AgendaTelefonica agenda = new AgendaTelefonica();
+
+        while (true)
+        {
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Adicionar contato");
+            Console.WriteLine("2 - Remover contato");
+            Console.WriteLine("3 - Buscar contato");
+            Console.WriteLine("4 - Sair");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Digite o nome do contato:");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine("Digite o telefone do contato:");
+                    string telefone = Console.ReadLine();
+                    Console.WriteLine("Digite o email do contato:");
+                    string email = Console.ReadLine();
+                    agenda.AdicionarContato(nome, telefone, email);
+                    break;
+                case "2":
+                    Console.WriteLine("Digite o nome do contato que deseja remover:");
+                    nome = Console.ReadLine();
+                    agenda.RemoverContato(nome);
+                    break;
+                case "3":
+                    Console.WriteLine("Digite o nome do contato que deseja buscar:");
+                    nome = Console.ReadLine();
+                    agenda.BuscarContato(nome);
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
