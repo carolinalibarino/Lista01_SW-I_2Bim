2dc58dea0f5e6789f3ab560c66f3a0a0498cdd8e
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-16 08:49:13 -0300

    Add files via upload

---
+++ exe05/GerenciadorDeTarefas.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe05
+{
+    class Tarefa
+    {
+        public string Descricao { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+        public Tarefa(string descricao, DateTime dataVencimento)
+        {
+            Descricao = descricao;
+            DataVencimento = dataVencimento;
+        }
+    }
+
+    class GerenciadorDeTarefas
+    {
+        private List<Tarefa> tarefas;
+
+        public GerenciadorDeTarefas()
+        {
+            tarefas = new List<Tarefa>();
+        }
+
+        public void AdicionarTarefa(string descricao, DateTime dataVencimento)
+        {
+            Tarefa tarefa = new Tarefa(descricao, dataVencimento);
+            tarefas.Add(tarefa);
+            Console.WriteLine("Tarefa adicionada com sucesso!");
+        }
+
+        public void RemoverTarefa(string descricao)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.Descricao == descricao);
+            if (tarefa != null)
+            {
+                tarefas.Remove(tarefa);
+                Console.WriteLine("Tarefa removida com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Tarefa não encontrada.");
+            }
+        }
+
+        public void ListarTarefas()
+        {
+            Console.WriteLine("Lista de Tarefas:");
+            foreach (Tarefa tarefa in tarefas)
+            {
+                Console.WriteLine($"Descrição: {tarefa.Descricao}, Data de Vencimento: {tarefa.DataVencimento.ToShortDateString()}");
+            }
+        }
+
+        public bool TarefaParaHoje(string descricao)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.Descricao == descricao);
+            if (tarefa != null)
+            {
+                return tarefa.DataVencimento.Date == DateTime.Today;
+            }
+            return false;
+        }
+    }
+}
---
+++ exe05/Program.cs
@@ -0,0 +1,59 @@
+﻿using exe05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
+
+        while (true)
+        {
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Adicionar tarefa");
+            Console.WriteLine("2 - Remover tarefa");
+            Console.WriteLine("3 - Listar tarefas");
+            Console.WriteLine("4 - Verificar se há tarefas para hoje");
+            Console.WriteLine("5 - Sair");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Digite a descrição da tarefa:");
+                    string descricao = Console.ReadLine();
+                    Console.WriteLine("Digite a data de vencimento da tarefa (formato: dd/mm/aaaa):");
+                    DateTime dataVencimento = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+                    gerenciador.AdicionarTarefa(descricao, dataVencimento);
+                    break;
+                case "2":
+                    Console.WriteLine("Digite a descrição da tarefa que deseja remover:");
+                    descricao = Console.ReadLine();
+                    gerenciador.RemoverTarefa(descricao);
+                    break;
+                case "3":
+                    gerenciador.ListarTarefas();
+                    break;
+                case "4":
+                    Console.WriteLine("Digite a descrição da tarefa que deseja verificar:");
+                    descricao = Console.ReadLine();
+                    bool paraHoje = gerenciador.TarefaParaHoje(descricao);
+                    if (paraHoje)
+                    {
+                        Console.WriteLine("Esta tarefa deve ser executada hoje.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Esta tarefa não deve ser executada hoje.");
+                    }
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
