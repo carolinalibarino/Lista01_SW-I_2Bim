31ae30fbd7d746316f3b1f9df73b6beb4390fa67
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-16 11:50:29 -0300

    Add files via upload

---
+++ exe10/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe10
+{
+    class Animal
+    {
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public int Idade { get; set; }
+        public string Som { get; set; }
+
+        public Animal(string nome, string especie, int idade, string som)
+        {
+            Nome = nome;
+            Especie = especie;
+            Idade = idade;
+            Som = som;
+        }
+
+        public void EmitirSom()
+        {
+            Console.WriteLine($"O som do {Nome} é: {Som}");
+        }
+    }
+}
---
+++ exe10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exe10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite as informações do animal:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Espécie: ");
+            string especie = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Som: ");
+            string som = Console.ReadLine();
+
+            Animal animal = new Animal(nome, especie, idade, som);
+
+            Console.WriteLine("\nInformações do animal:");
+            Console.WriteLine($"Nome: {animal.Nome}");
+            Console.WriteLine($"Espécie: {animal.Especie}");
+            Console.WriteLine($"Idade: {animal.Idade}");
+
+            animal.EmitirSom();
+        }
+    }
+}
