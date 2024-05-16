0692596b8bff78ac81fe32ad7cc3d348e0d8a0b3
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-16 08:30:34 -0300

    Add files via upload

---
+++ exe03/CorretotaDeImoveis.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe03
+{
+    class Imovel
+    {
+        public string Endereco { get; set; }
+        public double Preco { get; set; }
+        public string Tipo { get; set; }
+
+        public Imovel(string endereco, double preco, string tipo)
+        {
+            Endereco = endereco;
+            Preco = preco;
+            Tipo = tipo;
+        }
+    }
+
+    class CorretoraDeImoveis
+    {
+        private List<Imovel> imoveis;
+
+        public CorretoraDeImoveis()
+        {
+            imoveis = new List<Imovel>();
+        }
+
+        public void AdicionarImovel(string endereco, double preco, string tipo)
+        {
+            Imovel imovel = new Imovel(endereco, preco, tipo);
+            imoveis.Add(imovel);
+            Console.WriteLine("Imóvel adicionado com sucesso!");
+        }
+
+        public void AlterarPreco(string endereco, double novoPreco)
+        {
+            Imovel imovel = imoveis.Find(i => i.Endereco == endereco);
+            if (imovel != null)
+            {
+                imovel.Preco = novoPreco;
+                Console.WriteLine("Preço do imóvel alterado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Imóvel não encontrado.");
+            }
+        }
+
+        public void ListarImoveis()
+        {
+            Console.WriteLine("Lista de imóveis disponíveis:");
+            foreach (Imovel imovel in imoveis)
+            {
+                Console.WriteLine($"Endereço: {imovel.Endereco}, Preço: {imovel.Preco}, Tipo: {imovel.Tipo}");
+            }
+        }
+
+        public double CalcularValorMedio()
+        {
+            if (imoveis.Count == 0)
+            {
+                Console.WriteLine("Não há imóveis disponíveis para calcular o valor médio.");
+                return 0;
+            }
+            double somaPrecos = imoveis.Sum(i => i.Preco);
+            return somaPrecos / imoveis.Count;
+        }
+    }
+}
---
+++ exe03/Program.cs
@@ -0,0 +1,48 @@
+﻿using exe03;
+
+CorretoraDeImoveis corretora = new CorretoraDeImoveis();
+
+while (true)
+{
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1 - Inserir imóvel");
+    Console.WriteLine("2 - Alterar preço do imóvel");
+    Console.WriteLine("3 - Listar todos os imóveis");
+    Console.WriteLine("4 - Calcular valor médio dos imóveis");
+    Console.WriteLine("5 - Sair");
+
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Digite o endereço do imóvel:");
+            string endereco = Console.ReadLine();
+            Console.WriteLine("Digite o preço do imóvel:");
+            double preco = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o tipo do imóvel:");
+            string tipo = Console.ReadLine();
+            corretora.AdicionarImovel(endereco, preco, tipo);
+            break;
+        case "2":
+            Console.WriteLine("Digite o endereço do imóvel cujo preço será alterado:");
+            endereco = Console.ReadLine();
+            Console.WriteLine("Digite o novo preço:");
+            double novoPreco = Convert.ToDouble(Console.ReadLine());
+            corretora.AlterarPreco(endereco, novoPreco);
+            break;
+        case "3":
+            corretora.ListarImoveis();
+            break;
+        case "4":
+            double valorMedio = corretora.CalcularValorMedio();
+            Console.WriteLine($"O valor médio dos imóveis é: {valorMedio}");
+            break;
+        case "5":
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+}
