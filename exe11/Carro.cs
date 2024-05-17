bc2ba936ea4a4e5fc29936a39e3830f3bc911cd3
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-17 15:31:30 -0300

    Add files via upload

---
+++ exe11/Carro.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class Carro
+{
+    private string modelo;
+    private int ano;
+    private int velocidade;
+
+    public string Modelo
+    {
+        get => modelo;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("O modelo do carro não pode ser vazio.");
+            }
+            modelo = value;
+        }
+    }
+
+    public int Ano
+    {
+        get => ano;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("O ano do carro deve ser um valor positivo.");
+            }
+            ano = value;
+        }
+    }
+
+    public int Velocidade
+    {
+        get => velocidade;
+        private set
+        {
+            if (value < 0)
+            {
+                velocidade = 0;
+            }
+            else
+            {
+                velocidade = value;
+            }
+        }
+    }
+
+    public Carro(string modelo, int ano)
+    {
+        Modelo = modelo;
+        Ano = ano;
+        Velocidade = 0;
+    }
+
+    public void Acelerar()
+    {
+        Velocidade += 10;
+        Console.WriteLine($"O carro {Modelo} acelerou. Velocidade atual: {Velocidade} km/h.");
+    }
+
+    public void Frear()
+    {
+        Velocidade -= 10;
+        if (Velocidade < 0) Velocidade = 0;
+        Console.WriteLine($"O carro {Modelo} freou. Velocidade atual: {Velocidade} km/h.");
+    }
+}
---
+++ exe11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exe11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Carro carro1 = new Carro("Fusca", 1972);
+                carro1.Acelerar();
+                carro1.Acelerar();
+                carro1.Frear();
+                carro1.Frear();
+                carro1.Frear();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
