f913b5b690c7c2185b3d60da2bf7f8bb3eac5c06
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-17 15:13:19 -0300

    Add files via upload

---
+++ exe07/Playlist.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Musica
+{
+    public string Nome { get; set; }
+    public string Autor { get; set; }
+    public string Gravadora { get; set; }
+
+    public Musica(string nome, string autor, string gravadora)
+    {
+        Nome = nome;
+        Autor = autor;
+        Gravadora = gravadora;
+    }
+}
+
+public class Playlist
+{
+    private List<Musica> musicas;
+    public string Dono { get; set; }
+    private Random random;
+
+    public Playlist(string dono)
+    {
+        Dono = dono;
+        musicas = new List<Musica>();
+        random = new Random();
+    }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+        Console.WriteLine($"Música '{musica.Nome}' adicionada à playlist.");
+    }
+
+    public void TocarMusica()
+    {
+        if (musicas.Count == 0)
+        {
+            Console.WriteLine("A playlist está vazia.");
+            return;
+        }
+
+        int index = random.Next(musicas.Count);
+        Musica musica = musicas[index];
+        Console.WriteLine($"Tocando agora: {musica.Nome} - {musica.Autor}");
+    }
+}
---
+++ exe07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exe07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playlist minhaPlaylist = new Playlist("João");
+
+            Musica musica1 = new Musica("Imagine", "John Lennon", "Apple Records");
+            Musica musica2 = new Musica("Bohemian Rhapsody", "Queen", "EMI");
+            Musica musica3 = new Musica("Hotel California", "Eagles", "Asylum Records");
+
+            minhaPlaylist.AdicionarMusica(musica1);
+            minhaPlaylist.AdicionarMusica(musica2);
+            minhaPlaylist.AdicionarMusica(musica3);
+
+            Console.WriteLine("Tocando músicas aleatoriamente:");
+            minhaPlaylist.TocarMusica();
+            minhaPlaylist.TocarMusica();
+            minhaPlaylist.TocarMusica();
+        }
+    }
+}
