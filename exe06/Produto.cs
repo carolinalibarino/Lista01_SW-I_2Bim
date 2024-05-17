73d76f68d93467826951d496791a105fd596018a
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-05-17 12:36:46 -0300

    Add files via upload

---
+++ exe06/Produto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe06
+{
+
+    class Produto
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+
+        public Produto(string codigo, string nome, double preco, int quantidadeEmEstoque)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+            QuantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        public void AdicionarEstoque(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                QuantidadeEmEstoque += quantidade;
+                Console.WriteLine($"{quantidade} unidades do produto {Nome} foram adicionadas ao estoque.");
+            }
+            else
+            {
+                Console.WriteLine("A quantidade a ser adicionada deve ser maior que zero.");
+            }
+        }
+
+        public void RemoverEstoque(int quantidade)
+        {
+            if (quantidade > 0 && quantidade <= QuantidadeEmEstoque)
+            {
+                QuantidadeEmEstoque -= quantidade;
+                Console.WriteLine($"{quantidade} unidades do produto {Nome} foram removidas do estoque.");
+            }
+            else if (quantidade > QuantidadeEmEstoque)
+            {
+                Console.WriteLine("A quantidade a ser removida excede o estoque disponível.");
+            }
+            else
+            {
+                Console.WriteLine("A quantidade a ser removida deve ser maior que zero.");
+            }
+        }
+    }
+}
---
+++ exe06/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace exe06
+{
+    class Program
+    {
+        static List<Produto> listaProdutos = new List<Produto>();
+
+        static void Main(string[] args)
+        {
+            // Adicionando alguns produtos à lista
+            AdicionarProduto("P001", "Camiseta", 29.99, 50);
+            AdicionarProduto("P002", "Calça Jeans", 59.99, 30);
+            AdicionarProduto("P003", "Tênis Esportivo", 99.99, 20);
+
+            // Exibindo todos os produtos na tela
+            ExibirTodosProdutos();
+
+            // Consultando um produto pelo código
+            ConsultarProdutoPorCodigo("P002");
+        }
+
+        static void AdicionarProduto(string codigo, string nome, double preco, int quantidade)
+        {
+            Produto novoProduto = new Produto(codigo, nome, preco, quantidade);
+            listaProdutos.Add(novoProduto);
+        }
+
+        static void ExibirTodosProdutos()
+        {
+            Console.WriteLine("Lista de Produtos:");
+            foreach (var produto in listaProdutos)
+            {
+                Console.WriteLine($"Código: {produto.Codigo}, Nome: {produto.Nome}, Preço: {produto.Preco:C}, Quantidade em Estoque: {produto.QuantidadeEmEstoque}");
+            }
+        }
+
+        static void ConsultarProdutoPorCodigo(string codigo)
+        {
+            Produto produtoEncontrado = listaProdutos.Find(produto => produto.Codigo == codigo);
+            if (produtoEncontrado != null)
+            {
+                Console.WriteLine($"Produto encontrado - Código: {produtoEncontrado.Codigo}, Nome: {produtoEncontrado.Nome}, Preço: {produtoEncontrado.Preco:C}, Quantidade em Estoque: {produtoEncontrado.QuantidadeEmEstoque}");
+            }
+            else
+            {
+                Console.WriteLine("Produto não encontrado.");
+            }
+        }
+    }
+}
